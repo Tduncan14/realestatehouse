3f3d28be1ce604bab021f1e5f59e9cab43e124f7
tduncan14 <nbatre1@yahoo.com>
2022-09-10 14:04:32 -0700

    keep moving forward

--- src/components/Home/recent/RecentCard.js
+++ src/components/Home/recent/RecentCard.js
@@ -6,13 +6,22 @@ const RecentCard = () => {
     <div>
         <div className="content grid3 mtop">
             {list.map((val,index) =>{
-                const{cover,catergory,location,name,price,type} = val;
+                const{cover,category,location,name,price,type} = val;
 
 
                 return(
                     <div className="box shadow" key="index">
                         <div className="img">
                             <img src={cover} alt="" />
+                        </div>
+
+                        <div className="text">
+                            <div className="category flex">
+                             <span style={{background: category === 'For Sale' ? "#25b5791a" : "#ff98001a",color:category === "For Sale" ? "#25b579" : "#ff9800"}}>
+                            {category} 
+                                </span>
+
+                            </div>
                         </div>
                     </div>
                 )
