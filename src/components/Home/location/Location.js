d286d5901612e819c8b6a38ec39c7e156f5443c4
tduncan14 <nbatre1@yahoo.com>
2022-09-12 23:45:28 -0700

    keep moving forward

--- src/components/Home/location/Location.js
+++ src/components/Home/location/Location.js
@@ -23,6 +23,17 @@ const Location = () => {
                 return(
                 <div className="box">
                       <img src={items.cover} alt=''/>
+
+
+                      <div className="overlay">
+                        <h5>{items.name}</h5>
+
+                        <p>
+                        <label>{items.Villas}</label>
+                        <label>{items.Offices}</label>
+                        <label>{items.Apartments}</label>
+                        </p>
+                      </div>
                 </div>
               )})}
            </div>
